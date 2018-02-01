e4a69e76344adacf3cb14f5af07511bd68268cd2
Tillmann Heigel <heigel@tillmannheigel.de>
2018-02-01 22:28:31 +0100

    initialize repository with spring starter and web dependencies

---
+++ src/test/java/de/tillmannheigel/examples/EnumInController/EnumInControllerApplicationTests.java
@@ -0,0 +1,16 @@
+package de.tillmannheigel.examples.EnumInController;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnumInControllerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
